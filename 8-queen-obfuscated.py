be1d19039b9ab337dcfc4ad1ad501caa80b9c30a
Mohamad Badiezadegan <mbzadegan@gmail.com>
2026-08-16 20:59:17 -0700

    Add diagonal check for 8-queen solution

--- 8-queen-obfuscated.py
+++ 8-queen-obfuscated.py
@@ -1,5 +1,6 @@
 # (0, 4, 7, 5, 2, 6, 1, 3) means:
 # queens at (row,column) positions (0,0), (1,4), (2,7), ....
 # The trick is rather elegant. Because permutations(range(8)) already guarantees one queen per row and one per column, we only have to check the two diagonals.
+
 from itertools import permutations
 print(*[p for p in permutations(range(8)) if len({p[i]-i for i in range(8)})==len({p[i]+i for i in range(8)})==8],sep="\n")
