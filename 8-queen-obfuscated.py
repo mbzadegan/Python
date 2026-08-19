9e30b896ecd874b64846fc51dbe931195050d81c
Mohamad Badiezadegan <mbzadegan@gmail.com>
2026-08-18 18:50:59 -0700

    Update 8-queen-obfuscated.py

--- 8-queen-obfuscated.py
+++ 8-queen-obfuscated.py
@@ -1,5 +1,5 @@
 # (0, 4, 7, 5, 2, 6, 1, 3) means:
-# queens at (row,column) positions (0,0), (1,4), (2,7), ....
+# queens at (row,column) positions (0,0), (1,4), (2,7), ..
 # The trick is rather elegant. Because permutations(range(8)) already guarantees one queen per row and one per column, we only have to check the two diagonals.
 
 from itertools import permutations
